372017882733012fc4efaf774a6c4f6a96dc4e3f
DiegoFNav <diegonavarro12703@gmail.com>
2024-03-29 20:01:58 -0700

    Added some table styling.

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
+//test
 function App() {
   const [searchText, setSearchText] = useState('');
   const [pokemonList, setPokemonList] = useState([]);

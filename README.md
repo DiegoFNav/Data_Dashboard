ab59a8d06a329655b5f428ac9d2b02fcdbc0cca8
DiegoFNav <diegonavarro12703@gmail.com>
2024-03-30 01:35:53 -0700

    Finished ReadMe

--- README.md
+++ README.md
@@ -1,21 +1,23 @@
-# Web Development Project 5 - _Name of App Here_
+# Web Development Project 5 - Pokemon Search Dashboard
 
-Submitted by: **Your Name Here**
+Submitted by: Diego Navarro
 
-This web app: **insert description**
+This web app: takes in a search string and displays all pokemone with that search string in their name.
 
-Time spent: **X** hours spent in total
+Time spent: 7 hours spent in total
 
 ## Required Features
 
 The following **required** functionality is completed:
 
-- [ ] **The list displays a list of data fetched using an API call**
-- [ ] **Data uses the useEffect React hook and async/await syntax**
-- [ ] **The app dashboard includes at least three summary statistics about the data such as**
-  - [ ] _insert details_
-- [ ] **A search bar allows the user to search for an item in the fetched data**
-- [ ] **Multiple different filters (2+) allow the user to filter items in the database by specified categories**
+- [x] **The list displays a list of data fetched using an API call**
+- [x] **Data uses the useEffect React hook and async/await syntax**
+- [x] **The app dashboard includes at least three summary statistics about the data such as**
+  - [x] Total number of Pokemon
+  - [x] Average Height
+  - [x] Average Weight
+- [x] **A search bar allows the user to search for an item in the fetched data**
+- [x] **Multiple different filters (2+) allow the user to filter items in the database by specified categories**
 
 The following **optional** features are implemented:
 
@@ -31,24 +33,19 @@ The following **additional** features are implemented:
 
 Here's a walkthrough of implemented user stories:
 
-<img src='http://i.imgur.com/link/to/your/gif/file.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
+<img src='HW.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
 
-<!-- Replace this with whatever GIF tool you used! -->
-
-GIF created with ...
-
-<!-- Recommended tools:
-[Kap](https://getkap.co/) for macOS
-[ScreenToGif](https://www.screentogif.com/) for Windows
-[peek](https://github.com/phw/peek) for Linux. -->
+GIF created with LiceCap
 
 ## Notes
 
 Describe any challenges encountered while building the app.
 
+The biggest challenge was navigating the documentation of the API. Often I found myself lost on what my data was doing causing lots of misunderstandings.
+
 ## License
 
-    Copyright [yyyy] [name of copyright owner]
+    Copyright 2024 Diego Navarro
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -6,6 +6,10 @@ function App() {
   const [searchText, setSearchText] = useState('');
   const [pokemonList, setPokemonList] = useState([]);
   const [numToShow, setNumToShow] = useState(30);
+  const [total, setTotal] = useState(30);
+  const [avgWeight, setAvgWeight] = useState(0);
+  const [avgHeight, setAvgHeight] = useState(0);
+  const [sortBy, setSortBy] = useState('Name');
 
   useEffect(() => {
     fetchData();
@@ -21,6 +25,9 @@ function App() {
       const firstXPokemon = data.results.slice(0, numToShow);
       const pokemonDataList = await Promise.all(firstXPokemon.map(pokemon => getPokemonData(pokemon.url)));
       setPokemonList(pokemonDataList);
+      setAvgHeight(pokemonList.reduce((acc, pokemon) => acc + pokemon.height, 0) / pokemonList.length);
+      setAvgWeight(pokemonList.reduce((acc, pokemon) => acc + pokemon.weight, 0) / pokemonList.length);
+      setTotal(pokemonList.length);
     } catch (error) {
       console.error(error);
     }
@@ -33,10 +40,23 @@ function App() {
         throw new Error('Failed to fetch data');
       }
       const data = await response.json();
-      const filtered = data.results.filter(pokemon => pokemon.name.includes(searchText.toLowerCase()));
+      const pokemonDataList = await Promise.all(data.results.map(pokemon => getPokemonData(pokemon.url)));
+      let filtered = [];
+      if (sortBy === 'Name') {
+        filtered = pokemonDataList.filter(pokemon => pokemon.name.includes(searchText.toLowerCase()));
+      } else if (sortBy === 'Type') {
+        filtered = pokemonDataList.filter(pokemon => pokemon.types[0].type.name.includes(searchText.toLowerCase()) || (pokemon.types.length > 1 && pokemon.types[1].type.name.includes(searchText.toLowerCase())));
+      } else if (sortBy === 'Height') {
+        filtered = pokemonDataList.filter(pokemon => pokemon.height >= parseInt(searchText) * 10);
+      }
+      if (filtered.length === 0) {
+        console.log('No results found');
+      }
       const firstXPokemon = filtered.slice(0, numToShow);
-      const pokemonDataList = await Promise.all(firstXPokemon.map(pokemon => getPokemonData(pokemon.url)));
-      setPokemonList(pokemonDataList);
+      setPokemonList(firstXPokemon);
+      setAvgHeight(firstXPokemon.reduce((acc, pokemon) => acc + pokemon.height, 0) / pokemonList.length);
+      setAvgWeight(firstXPokemon.reduce((acc, pokemon) => acc + pokemon.weight, 0) / pokemonList.length);
+      setTotal(firstXPokemon.length);
     } catch (error) {
       console.error(error);
     }
@@ -44,6 +64,10 @@ function App() {
 
   const handleChange = event => {
     setSearchText(event.target.value);
+  };
+
+  const handleSortChange = event => {
+    setSortBy(event.target.value);
   };
 
   const getPokemonData = async url => {
@@ -67,8 +91,28 @@ function App() {
   return (
     <div className='main_container'>
       <div className='heading_container'><h1>Pokemon Search Dashboard</h1></div>
+      <div className='search_container'>
+        <label htmlFor="sortBy">Search By:</label>
+        <select id="sortBy" onChange={handleSortChange} value={sortBy}>
+          <option value="Name">Name</option>
+          <option value="Type">Type</option>
+          <option value="Height">Min Height</option>
+        </select>
+      </div>
       <input type="text" value={searchText} onChange={handleChange} />
       <button className="button" onClick={handleSearch}>Search</button>
+      <div className='statistics_container'>
+        <div className='stat_card'>
+          <h3>Total Pokemon: {total}</h3>
+        </div>
+        <div className='stat_card'>
+          <h3>Average Height: {parseInt(avgHeight / Math.pow(10, 1))}m</h3>
+        </div>
+        <div className='stat_card'>
+          <h3>Average Weight: {parseInt(avgWeight / Math.pow(10, 1))}kg</h3>
+        </div>
+
+      </div>
       <table>
         <thead>
           <tr>
